334b068c377cb5992a9abf69bdc502399365ed61
Richard Hounslow <rhounslow@gmail.com>
2021-02-20 14:22:31 +1100

    Setup getlogs action and reducer

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,3 +1,5 @@
+import { GET_LOGS, SET_LOADING, LOGS_ERROR } from '../actions/types';
+
 const initialState = {
   logs: null,
   current: null,
@@ -7,6 +9,11 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
     default:
       return state;
   }
