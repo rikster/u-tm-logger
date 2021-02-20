4e80a7483d0c39eb8da44a96c42985d367446ee1
Richard Hounslow <rhounslow@gmail.com>
2021-02-20 14:26:46 +1100

    Refactor action

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,7 +1,21 @@
 import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
 
-export const getLogs = () => {
-  return async dispatch => {
+// export const getLogs = () => {
+//   return async dispatch => {
+//     setLoading();
+//
+//     const res = await fetch('/logs');
+//     const data = await res.json();
+//
+//     dispatch({
+//       type: GET_LOGS,
+//       payload: data
+//     });
+//   };
+// };
+
+export const getLogs = () => async dispatch => {
+  try {
     setLoading();
 
     const res = await fetch('/logs');
@@ -11,7 +25,12 @@ export const getLogs = () => {
       type: GET_LOGS,
       payload: data
     });
-  };
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    });
+  }
 };
 
 export const setLoading = () => {
