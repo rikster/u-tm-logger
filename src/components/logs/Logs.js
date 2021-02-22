83e43d09293239cd04a8cc32421afe7ce205d6cd
Richard Hounslow <rhounslow@gmail.com>
2021-02-22 13:38:36 +1100

    Init and setup Redux

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,44 +1,40 @@
-import React, {useEffect, useState} from 'react';
-import LogItem from "./LogItem";
-import Preloader from "../layout/Preloader";
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import LogItem from './LogItem';
+import Preloader from '../layout/Preloader';
+import PropTypes from 'prop-types';
+import { getLogs } from '../../actions/logActions';
 
-const Logs = () => {
-    const [logs, setLogs] = useState([]);
-    const [loading, setloading] = useState(false);
+const Logs = ({ log: { logs, loading}, getLogs  }) => {
+  useEffect(() => {
+    getLogs();
+    // eslint-disable-next-line
+  }, []);
 
-    useEffect(() => {
-        getLogs();
-        // eslint-disable-next-line
-    }, []);
+  if (loading || logs === null) {
+    return <Preloader />;
+  }
 
-    const getLogs = async () => {
-        setloading(true);
-
-        const res = await fetch('/logs'); //proxy in package.json will add API hostname
-        const data = await res.json(); // vs axios res.data
-        setLogs(data);
-
-        setloading(false);
-    }
-
-    if (loading) {
-        return (<Preloader/>);
-    }
-
-    return (
-        <div>
-            <ul className="collection with-header">
-                <li className="collection-header">
-                    <h4 className="center">Systems Logs</h4>
-                </li>
-                {!loading && logs.length === 0 ? (
-                    <p className='center'>No logs to show...</p>
-                ) : (
-                    logs.map(log => <LogItem log={log} key={log.id}/>)
-                )}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <ul className='collection with-header'>
+        <li className='collection-header'>
+          <h4 className='center'>Systems Logs</h4>
+        </li>
+        {!loading && logs.length === 0 ? (
+          <p className='center'>No logs to show...</p>
+        ) : (
+          logs.map(log => <LogItem log={log} key={log.id} />)
+        )}
+      </ul>
+    </div>
+  );
 };
 
-export default Logs;
+Logs.propTypes = {
+  log: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({ log: state.log });
+
+export default connect(mapStateToProps, { getLogs })(Logs);
--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -20,13 +20,12 @@ export default (state = initialState, action) => {
         ...state,
         loading: true
       };
-
     case LOGS_ERROR:
-      console.error(action.payload)
+      console.error(action.payload);
       return {
         ...state,
         error: action.payload
-      }
+      };
     default:
       return state;
   }
