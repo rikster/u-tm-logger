64304e2e878be1e6a68d6cbafcdc4d7ba6fc82bf
Richard Hounslow <rhounslow@gmail.com>
2021-02-23 13:43:16 +1100

    Update Logs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -4,16 +4,18 @@ import {
   LOGS_ERROR,
   ADD_LOG,
   DELETE_LOG,
-  DELETE_TECH
+  UPDATE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT
 } from './types';
 
 // export const getLogs = () => {
 //   return async dispatch => {
 //     setLoading();
-//
+
 //     const res = await fetch('/logs');
 //     const data = await res.json();
-//
+
 //     dispatch({
 //       type: GET_LOGS,
 //       payload: data
@@ -21,7 +23,7 @@ import {
 //   };
 // };
 
-// get logs from server
+// Get logs from server
 export const getLogs = () => async dispatch => {
   try {
     setLoading();
@@ -36,12 +38,12 @@ export const getLogs = () => async dispatch => {
   } catch (err) {
     dispatch({
       type: LOGS_ERROR,
-      payload: err.response.data
+      payload: err.response.statusText
     });
   }
 };
 
-// get logs from server
+// Add new log
 export const addLog = log => async dispatch => {
   try {
     setLoading();
@@ -62,12 +64,12 @@ export const addLog = log => async dispatch => {
   } catch (err) {
     dispatch({
       type: LOGS_ERROR,
-      payload: err.response.data
+      payload: err.response.statusText
     });
   }
 };
 
-// delete log from server
+// Delete log from server
 export const deleteLog = id => async dispatch => {
   try {
     setLoading();
@@ -83,13 +85,56 @@ export const deleteLog = id => async dispatch => {
   } catch (err) {
     dispatch({
       type: LOGS_ERROR,
-      payload: err.response.data
+      payload: err.response.statusText
     });
   }
 };
 
+// Update log on server
+export const updateLog = log => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs/${log.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await res.json();
+
+    dispatch({
+      type: UPDATE_LOG,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.statusText
+    });
+  }
+};
+
+// Set current log
+export const setCurrent = log => {
+  return {
+    type: SET_CURRENT,
+    payload: log
+  };
+};
+
+// Clear current log
+export const clearCurrent = () => {
+  return {
+    type: CLEAR_CURRENT
+  };
+};
+
+// Set loading to true
 export const setLoading = () => {
-    return {
-        type: SET_LOADING
-    };
+  return {
+    type: SET_LOADING
+  };
 };
--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,16 +1,41 @@
 import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import M from 'materialize-css/dist/js/materialize.min.js';
+import { updateLog } from '../../actions/logActions';
 
-const EditLogModal = () => {
+const EditLogModal = ({ current, updateLog }) => {
   const [message, setMessage] = useState('');
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState('');
+
+  useEffect(() => {
+    if (current) {
+      setMessage(current.message);
+      setAttention(current.attention);
+      setTech(current.tech);
+    }
+  }, [current]);
 
   const onSubmit = () => {
     if (message === '' || tech === '') {
       M.toast({ html: 'Please enter a message and tech' });
     } else {
-      console.log('Message tech and attention');
+      const updLog = {
+        id: current.id,
+        message,
+        attention,
+        tech,
+        date: new Date()
+      };
+
+      updateLog(updLog);
+      M.toast({ html: `Log updated by ${tech}` });
+
+      // Clear Fields
+      setMessage('');
+      setTech('');
+      setAttention(false);
     }
   };
 
@@ -82,4 +107,13 @@ const modalStyle = {
   height: '75%'
 };
 
-export default EditLogModal;
+EditLogModal.propTypes = {
+  current: PropTypes.object,
+  updateLog: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  current: state.log.current
+});
+
+export default connect(mapStateToProps, { updateLog })(EditLogModal);
